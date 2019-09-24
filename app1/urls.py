fd7a65b701d64a2a55c37c4f5fa5fe2625ceb125
jion-kozono <zeon66190329@gmail.com>
2019-09-24 21:49:55 +0900

    'home' -> '' 2019_09_24

--- app1/urls.py
+++ app1/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import HomePageView, AboutPageView, BooksPageView, booksMenuPageView
 
 urlpatterns = [
-    path('home', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
     path('about', AboutPageView.as_view(), name='about'),
     path('books', BooksPageView.as_view(), name='books'),
     path('booksmenu', booksMenuPageView.as_view(), name='booksmenu')
